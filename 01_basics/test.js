c388eef395913421008e126baacf3bbd7e1c9fc4
Mayank11989 <mayankkatiyar269@gmail.com>
2025-09-03 09:56:05 +0000

    container config

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,14 @@
-console.log("Mayank")
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 not allowed 
+
+accountEmail = "hcQhc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+console.table(accountId,accountEmail,accountPassword,accountCity);
