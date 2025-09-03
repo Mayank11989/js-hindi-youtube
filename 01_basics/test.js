9ba932b7b7f1e127d1f505397e977434bdf87713
Mayank11989 <mayankkatiyar269@gmail.com>
2025-09-03 10:06:40 +0000

    container config

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -2,8 +2,14 @@ const accountId = 144553
 let accountEmail = "hitesh@google.com"
 var accountPassword = "12345"
 accountCity = "jaipur"
+let accountState;
 
 // accountId = 2 not allowed 
+
+/*
+prefer not to use var 
+beacause of issue in block scope and functional scope
+*/
 
 accountEmail = "hcQhc.com"
 accountPassword = "21212121"
@@ -11,4 +17,4 @@ accountCity = "Bengaluru"
 
 console.log(accountId);
 
-console.table([accountId,accountEmail,accountPassword,accountCity]);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
