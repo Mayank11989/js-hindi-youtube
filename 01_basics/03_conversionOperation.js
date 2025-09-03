5110fc77de2011e741b7e04eba2fb33be1e04205
Mayank11989 <mayankkatiyar269@gmail.com>
2025-09-03 11:37:23 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -25,3 +25,22 @@ let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+
+// ***************** operations ***********************
+
+let value = 3
+let negvalue = -value
+
+console.log(2+2)
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+
+console.log(str3)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
