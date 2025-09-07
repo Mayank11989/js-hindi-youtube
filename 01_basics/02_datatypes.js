c66c1772ebeb7bdd6799e56fec29494d49ae434a
Mayank11989 <mayankkatiyar269@gmail.com>
2025-09-07 05:01:22 +0000

    container config

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -16,6 +16,35 @@ let isLoggedIn = false
 
 // object 
 
-console.log(typeof "hitesh");
-console.log(typeof null);
+// console.log(typeof "hitesh");
+// console.log(typeof null);
 
+
+
+// Stack (Primitive)  , Heap (Non Primitive)
+
+
+let myYoutubename = "hiteshchaudharydotcom"
+
+let anothername = myYoutubename
+
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+
+console.log(anothername);
+
+
+
+
+let userOne = {
+    email: "user@google.com",
+    upi:"user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "hitesh@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
