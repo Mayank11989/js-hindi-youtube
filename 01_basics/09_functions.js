9df3e27a6b1021e7a48379241b213a835d9713f3
Mayank11989 <mayankkatiyar269@gmail.com>
2025-09-11 07:20:45 +0000

    function

--- 01_basics/09_functions.js
+++ 01_basics/09_functions.js
@@ -1,9 +1,9 @@
-console.log("M");
-console.log("a");
-console.log("y");
-console.log("a");
-console.log("n");
-console.log("k");
+// console.log("M");
+// console.log("a");
+// console.log("y");
+// console.log("a");
+// console.log("n");
+// console.log("k");
 
 function sayMyName(){
     console.log("M");
@@ -13,5 +13,21 @@ function sayMyName(){
     console.log("n");
     console.log("k");
 }
- 
-sayMyName()
+// sayMyName()
+
+
+
+// function addTwoNumbers (number1 , number2){
+//     console.log(number1 + number2);
+// }
+
+function addTwoNumbers (number1, number2){
+    let result = number1 + number2
+    return result
+}
+
+const result = addTwoNumbers(3 , 5)
+
+console.log( "result" , result);
+
+
