a07e4a48811a3dd2d2598e1dc3db28f6680ad4db
Mayank11989 <mayankkatiyar269@gmail.com>
2025-09-11 10:28:00 +0000

    scope

--- 01_basics/09_functions.js
+++ 01_basics/09_functions.js
@@ -22,12 +22,53 @@ function sayMyName(){
 // }
 
 function addTwoNumbers (number1, number2){
-    let result = number1 + number2
-    return result
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
 }
 
-const result = addTwoNumbers(3 , 5)
+// const result = addTwoNumbers(3 , 5)
 
-console.log( "result" , result);
+// console.log( "result" , result);
+
+function loginUserMessage(username = "sam"){
+    if(!username){
+        console.log("please enter a user name")
+        return
+    }
+    return `${username} just logged in `
+}
+// console.log(loginUserMessage())
 
 
+
+function calculateCartPrice(...num1){
+    return num1 
+}
+ 
+// console.log(calculateCartPrice(200,400,500 , 2000 ))
+
+const user = {
+    username: "hitesh",
+    price: 199 
+}
+function handleObject(anyobject){
+    console.log(`User name is ${anyobject.username} and the price is ${anyobject.price}`);
+}
+
+// handleObject(user)
+
+// handleObject({
+//     username: "sam",
+//     price: 399
+// })
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+// console.log(returnSecondValue(myNewArray));
+
+console.log(returnSecondValue([200,400,500,1000]))
