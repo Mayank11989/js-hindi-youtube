7d43fa56e128cfdd5bf5e69cd8f852efa7903968
Mayank11989 <mayankkatiyar269@gmail.com>
2025-09-18 06:53:52 +0000

    three

--- 03_iterations/two.js
+++ 03_iterations/two.js
@@ -12,9 +12,12 @@
 // }
 
 
-let score = 11
+// let score = 11
 
-do {
-   console.log(`score is ${score}`)
-   score++
-} while (score <= 10);
+// do {
+//    console.log(`score is ${score}`)
+//    score++
+// } while (score <= 10);
+
+
+
